fd86c0cfb49d428253fb06cfd32e2322965b84b1
Felipe Mendes <fmagliom@gmail.com>
2021-09-01 14:13:07 -0300

    update text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import { Marketing } from './components/Marketing';
 const App = () => {
     return (
         <>
-            <h1>Hi</h1>
+            <h1>Hi!</h1>
             <hr />
             <Marketing />
         </>
